a141fdabcd9523e73ade552e718e6f40d1988e2e
NelonArrive <nelon.arrive@gmail.com>
2025-10-09 17:13:40 +0500

    🔅 added changes in abstract-classes

--- abstract-classes/src/Main.java
+++ abstract-classes/src/Main.java
@@ -1,12 +1,51 @@
 public class Main {
 	public static void main(String[] args) {
-	
+		User admin = new Admin("Алексей");
+		User client = new Client("Мария");
+		
+		admin.login(); // Алексей вошёл в систему ✅
+		admin.accessDashboard(); // Алексей открыл админ-панель ⚙️
+		
+		client.login(); // Мария вошёл в систему ✅
+		client.accessDashboard(); // Мария открыл клиентский кабинет 💼
 	}
 }
 
-//git init
-//git add README.md
-//git commit -m "first commit"
-//git branch -M main
-//git remote add origin https://github.com/NelonArrive/java-course.git
-//git push -u origin main
+
+abstract class User {
+	String name;
+	
+  // Constructor
+	User(String name) {
+		this.name = name;
+	}
+	
+	abstract void accessDashboard(); // каждый тип пользователя реализует по-своему
+	
+	void login() {
+		System.out.println(name + " вошёл в систему ✅");
+	}
+}
+
+class Admin extends User {
+	Admin(String name) {
+		super(name);
+	}
+	
+	@Override
+	void accessDashboard() {
+		System.out.println(name + " открыл админ-панель ⚙️");
+	}
+}
+
+class Client extends User {
+	Client(String name) {
+		super(name);
+	}
+	
+	@Override
+	void accessDashboard() {
+		System.out.println(name + " открыл клиентский кабинет 💼");
+	}
+}
+
